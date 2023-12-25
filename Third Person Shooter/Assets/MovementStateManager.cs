8a0092e54043e2df765329fcb228722ace5d0606
kaneki <olegsapozhnik6699@gmail.com>
2023-12-25 11:59:49 +0200

    Create gravity

--- Third Person Shooter/Assets/MovementStateManager.cs
+++ Third Person Shooter/Assets/MovementStateManager.cs
@@ -6,9 +6,15 @@ public class MovementStateManager : MonoBehaviour
 {
     public float moveSpeed = 3;
     [HideInInspector] public Vector3 dir;
-    public float hzInput, vInput;
+    private float hzInput, vInput;
     CharacterController controller;
 
+    [SerializeField] private float groundYOffset;
+    [SerializeField] private LayerMask groundMask;
+    Vector3 spherePos;
+
+    [SerializeField] float gravity =- 9.81f;
+    Vector3 velocity;
     private void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -17,6 +23,7 @@ public class MovementStateManager : MonoBehaviour
     private void Update()
     {
         GetDirectionAndMove();
+        Gravity();
     }
 
     private void GetDirectionAndMove()
@@ -27,5 +34,35 @@ public class MovementStateManager : MonoBehaviour
         dir = transform.forward * vInput + transform.right * hzInput;
 
         controller.Move(dir * moveSpeed * Time.deltaTime);
+    }
+
+    private bool Ground()
+    {
+        spherePos = new Vector3(transform.position.x, transform.position.y * groundYOffset, transform.position.z);
+        if (Physics.CheckSphere(spherePos, controller.radius, groundMask))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void Gravity()
+    {
+        if (!Ground()) velocity.y += gravity * Time.deltaTime;
+        else if (velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(spherePos, controller.radius);
     }
 }
